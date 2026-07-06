1f72bad192b0c71d9170c16da1c833f1ed874481
Jennifer Taylor <jeataylor.2003@gmail.com>
2026-07-06 17:51:04 +0200

    days left works

--- static/js/listingTimer.js
+++ static/js/listingTimer.js
@@ -1,11 +1,8 @@
-const { response } = require("express")
 const daysLeft = document.getElementById("days-left");
-
-
     
 var today = new Date();
-var removalDate = new Date(job.removalDate)
-var difference = today.getTime() - removalDate.getTime() ;
+var removalDate = new Date(job.removalDate) ;
+var difference = removalDate.getTime() - today.getTime();
 var days = Math.ceil(difference / (1000 * 3600 * 24)) ;
 
 daysLeft.textContent = days;

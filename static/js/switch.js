c62f500b45e63c638c69b0ea3ccd040c0c5dadcf
xX_DarkLorD_Xx <yuzekesu@gmail.com>
2026-07-17 21:43:59 +0200

    add cookie feature, now the switch will remember the state for one hour.

--- static/js/switch.js
+++ static/js/switch.js
@@ -10,20 +10,48 @@ export class Switch extends HTMLElement {
             </section>
         `;
 
-        const thisSwitch = this.querySelector(".container.switch");
-        const slider = thisSwitch.querySelector(".slider.switch");
-        thisSwitch.addEventListener("click", () => {
+        const container = this.querySelector(".container.switch");
+        const slider = container.querySelector(".slider.switch");
+        container.addEventListener("click", () => {
+            this.isOn = !this.isOn;
             slider.classList.toggle("on"); /* add .css class to the element. */
         });
 
-        thisSwitch.querySelector(".text.left.switch").textContent = "Off";
-        thisSwitch.querySelector(".text.right.switch").textContent = "On";
+        container.querySelector(".text.left.switch").textContent = "Off";
+        container.querySelector(".text.right.switch").textContent = "On";
 
+
+        if (typeof document !== "undefined") {
+            const key = `${this.id}_isOn=`;
+            const part = document.cookie
+                .split("; ")
+                .find(row => row.startsWith(key));
+
+            if (part) {
+                const value = part.slice(key.length);
+                this._isOn = (value === "true");
+            }
+            else {
+                /* default value */
+                this._isOn = false;
+            }
+        }
         if (this._isOn) {
-            clickArea.click();
+            this._isOn = !this._isOn;
+            /* toggle .css class to the element. */
+            /* also bubble up to the parent, aka this class */
+            container.click();
         }
     }
-
+    set isOn(isTrue) {
+        this._isOn = isTrue;
+        if (typeof document !== "undefined") {
+            document.cookie = `${this.id}_isOn=${this.isOn}; path=/; max-age=3600`;
+        }
+    }
+    get isOn() {
+        return this._isOn;
+    }
     static get observedAttributes() { return ["lefttext", "righttext"]; }
 
     attributeChangedCallback(name, oldValue, newValue) {

19adcbd6d03c1622457b2ec6178b8bffad9a13e4
xX_DarkLorD_Xx <yuzekesu@gmail.com>
2026-07-24 02:04:55 +0200

    add cowork ability between containers.

--- database/redisClient.py
+++ database/redisClient.py
@@ -3,4 +3,11 @@ import redis
 class db:
     def db_connection():
         #connect to the db thats already running on redis
-        return redis.Redis(host="localhost", port=6379, decode_responses=True)
+        try:
+            client = redis.Redis(host="localhost", port=6379, decode_responses=True) 
+            client.ping()
+            return client
+        except redis.RedisError:
+            return redis.Redis(host="redis", port=6379, decode_responses=True) 
+
+        

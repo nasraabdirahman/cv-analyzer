1f81b53d7a506b0fdfdd49a9771e0421bca17e6c
nasraabdirahman <nasrafaisal72@gmail.com>
2026-07-26 18:36:43 +0200

    fixed some issues

--- tests/test_createEndpoint.py
+++ tests/test_createEndpoint.py
@@ -3,8 +3,8 @@
 def test_create_endpoint(client, mocker):
     fake_redis = mocker.Mock()
     fake_redis.incr.return_value = 1
-    #variable r on "jobCreation.py" is replaced by my fake redis
-    mocker.patch('services.jobCreation.r', fake_redis)
+    #variable r on "job.py" is replaced by my fake redis
+    mocker.patch('services.job.r', fake_redis)
     response = client.post("/create", data={
         "company": "Google",
         "title": "Developer",

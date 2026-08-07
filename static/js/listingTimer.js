7338a5e3af9ba231e454f1b153ebaf4476296ba5
Jennifer Taylor <jeataylor.2003@gmail.com>
2026-08-07 15:07:25 +0200

    changes for Yuze

--- static/js/listingTimer.js
+++ static/js/listingTimer.js
@@ -1,12 +1,10 @@
 export function timer(removal, today) {
     var removalDate = new Date(removal) ;
     var difference = removalDate.getTime() - today.getTime();
-    console.log(difference);
     return Math.ceil(difference / (1000 * 3600 * 24)) ;  
 }   
 
 export function displayTimer(job) {
     const daysLeft = document.getElementById("days-left");
     daysLeft.textContent = timer(job.removalDate, new Date()); 
-    console.log(daysLeft.textContent);
 }
